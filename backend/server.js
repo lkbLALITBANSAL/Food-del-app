1b785516623a78a31413ea2e436da721bb298b5c
L_K_B <lalitbansal8322@gmail.com>
2026-03-13 15:41:34 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -6,33 +6,34 @@ import userRouter from './routes/userRoutes.js'
 import 'dotenv/config.js'
 import cartRouter from './routes/cartRoutes.js'
 import orderRouter from './routes/orderRoutes.js'
- 
+
 //app config
-const app=express() 
-const port=4000
-     
+const app = express()
+const port = process.env.PORT || 4000
+
 //middleware
-app.use(express.json()) 
-app.use(cors())
+app.use(express.json())
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}))
 
 //DB connection
-connectDB();
-   
-//api endpoint ,   router
-app.use("/api/food",foodRouter) 
+connectDB()
+
+//api endpoint
+app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
-
-app.use("/api/cart",cartRouter);
+app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
 
 app.get("/",(req,res)=>{
     res.send("API working")
-})  
- 
-app.listen(port,()=>{
-    console.log(`server started on http://localhost:${port}`) 
 })
 
-// mongodb+srv://lalitbansal:8875228322@cluster0.m1zlvew.mongodb.net/?
-  
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`)
+})
