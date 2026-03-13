2b34a8c23d69f027335bf7402037c936a6b5261e
L_K_B <lalitbansal8322@gmail.com>
2026-03-13 17:29:27 +0530

    updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,8 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user oreder from frontend
 const placeOrder=async (req,res)=>{
 
-    const frontend_url="http://localhost:5173"
+   const frontend_url="https://food-del-app-lilac.vercel.app"
+
   try {
     const newOrder=new orderModel({
         userId:req.body.userId,
